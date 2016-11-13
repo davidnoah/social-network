7cfa648d1f16eaa1f45c72ba05c850eaa3100ae3
davidnoah <david.t.noah@gmail.com>
2016-11-13 15:51:01 -0800

    parse dictionary

--- social_network.rb
+++ social_network.rb
@@ -1,10 +1,22 @@
+require 'set'
+
 class Social_Network
   def initialize(word, dictionary)
     @word = word
-    @dictionary = dictionary
+    @dictionary = build_dictionary(dictionary)
   end
 
   def size
     
   end
+
+  def build_dictionary(dictionary)
+    set = Set.new
+    File.readlines(dictionary).each do |line|
+      set.add(line.chomp)
+    end
+    set
+  end
 end
+
+p Social_Network.new('LISTY', 'dict/very_small_test_dictionary.txt')
