b805e83f5f69621a2295f9e675a7c3df303ac26c
davidnoah <david.t.noah@gmail.com>
2016-11-13 17:50:32 -0800

    update

--- social_network.rb
+++ social_network.rb
@@ -1,28 +1,25 @@
 require 'set'
 
 class Social_Network
-  attr_reader :length, :word, :dictionary, :alpha
+  attr_reader :word, :dictionary, :alpha
   attr_accessor :count
 
   def initialize(word, dictionary)
-    @word = word
+    @words_queue = [word]
     @dictionary = build_dictionary(dictionary)
-    @length = word.length
     @alpha = ("A".."Z").to_a
     @count = 0
-    @words_queue = [word]
     @seen = Set.new.add(word)
   end
 
   def size
     until @words_queue.empty?
-      p @words_queue
-      check_count(@words_queue.shift)
+      count_friends(@words_queue.shift)
     end
     @count
   end
 
-  def check_count(current_word)
+  def count_friends(current_word)
     current_word.length.times do |idx|
       check_deletion(idx, current_word)
       check_addition(idx, current_word)
@@ -31,16 +28,18 @@ class Social_Network
   end
 
   def check_deletion(index, current_word)
-      temp = current_word[0...index] << current_word[(index + 1)...length]
-      check_dictionary(temp)
+    length = current_word.length
+    temp = current_word[0...index] + current_word[(index + 1)...length]
+    check_dictionary(temp)
   end
 
   def check_addition(index, current_word)
+    length = current_word.length
     alpha.each do |letter|
       temp = current_word[0, index] + letter + current_word[index...length]
       check_dictionary(temp)
 
-      if index == (length - 1)
+      if index == (current_word.length - 1)
         temp = current_word + letter
         check_dictionary(temp)
       end
@@ -48,8 +47,9 @@ class Social_Network
   end
 
   def check_substitution(index, current_word)
+    length = current_word.length
     alpha.each do |letter|
-      temp = current_word[0...index] << letter << current_word[(index + 1)...length]
+      temp = current_word[0...index] + letter + current_word[(index + 1)...length]
       check_dictionary(temp)
     end
   end
