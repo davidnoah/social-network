8604fbb39672fb2d0c15c47417fbf43e129aace4
davidnoah <david.t.noah@gmail.com>
2016-11-13 17:02:03 -0800

    cout check

--- social_network.rb
+++ social_network.rb
@@ -1,13 +1,67 @@
 require 'set'
 
 class Social_Network
+  attr_reader :length, :word, :dictionary, :alpha
+
   def initialize(word, dictionary)
     @word = word
     @dictionary = build_dictionary(dictionary)
+    @length = word.length
+    @alpha = ("A".."Z").to_a
   end
 
   def size
-    
+
+  end
+
+  def count_check
+    count = 0
+
+    length.times do |idx|
+
+    end
+  end
+
+  def check_deletions
+    count = 0
+
+    length.times do |idx|
+      temp = word[0...idx] << word[(idx + 1)...length]
+      if dictionary.include?(temp)
+        count += 1
+      end
+    end
+
+    count
+  end
+
+  def check_additions
+    count = 0
+
+    length.times do |idx|
+      alpha.each do |letter|
+        if idx == 0
+          temp = letter + word
+        else
+          temp = word[0..idx] + letter + word[idx...length]
+        end
+        if dictionary.include?(temp)
+          count += 1
+        end
+
+        if idx == (length - 1)
+          temp = word + letter
+          if dictionary.include?(temp)
+            count += 1
+          end
+        end
+      end
+    end
+
+    count
+  end
+
+  def check_substitutions
   end
 
   def build_dictionary(dictionary)
@@ -17,6 +71,7 @@ class Social_Network
     end
     set
   end
+
 end
 
-p Social_Network.new('LISTY', 'dict/very_small_test_dictionary.txt')
+p Social_Network.new('LISTY', 'dict/very_small_test_dictionary.txt').check_additions
